e645598d7b5f49930e1e85ff638e147098fe7848
ashigaru0 <ashigarua0@gmail.com>
2022-11-11 22:11:12 +0700

    work

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import add_window
 
 from PyQt5.QtWidgets import (QApplication,
                              QMainWindow,
-                             QTableWidgetItem)
+                             QTableWidgetItem,
+                             QMessageBox)
 from design.py.macros import Ui_MainWindow
 
 
@@ -18,22 +19,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.initUI()
 
     def initUI(self):
+        self.modified = {}
+        self.TITLES = {0: 'name', 1: 'combination', 2: 'file_name', 3: 'url_file'}
         self.add_combination()
         self.btn()
 
     def btn(self):
         self.add_btn.clicked.connect(self.add_macros)
+        self.del_btn.clicked.connect(self.del_macros)
+        self.change_btn.clicked.connect(self.change_macros)
+        self.macros_table.itemChanged.connect(self.item_changed)
+        self.macros_table.doubleClicked.connect(self.item_double_clicked)
 
     def add_macros(self):
         self.Window = add_window.AddWidget()
         self.Window.show()
         self.Window.login_data[list].connect(self.add_to_database)
 
-    def del_macros(self, data):
+    def del_macros(self):
         pass
 
     def change_macros(self):
-        pass
+        if self.modified:
+            con = sqlite3.connect('macros_db.sqlite')
+            cur = con.cursor()
+
+            for id in self.modified.keys():
+                que = "UPDATE macros SET\n"
+                que += f"{self.TITLES[self.modified[id][1]]} = '{self.modified[id][0]}'\n"
+                que += f'WHERE id = {id}'
+                print(que)
+                cur.execute(que)
+
+            con.commit()
+            con.close()
+            self.modified.clear()
+            self.table_update()
 
     def add_to_database(self, data):
         con = sqlite3.connect('macros_db.sqlite')
@@ -59,15 +80,40 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def table_update(self):
         con = sqlite3.connect('macros_db.sqlite')
         cur = con.cursor()
-        data = cur.execute("""SELECT id, name, combination, file_name FROM macros""").fetchall()
+        data = cur.execute("""SELECT name, combination, file_name, url_file FROM macros""").fetchall()
         con.close()
 
         self.macros_table.setRowCount(0)
         for i, row in enumerate(data):
             self.macros_table.setRowCount(self.macros_table.rowCount() + 1)
-            for j, elem in enumerate(row[1::]):
+            for j, elem in enumerate(row):
                 self.macros_table.setItem(i, j, QTableWidgetItem(elem))
-        self.macros_table.resizeColumnsToContents()
+
+    def item_double_clicked(self, item):
+        if item.column() == 1:
+            hotkey = self.add_comb()
+            self.modified[item.row() + 1] = [hotkey, item.column()]
+            self.macros_table.cha
+
+    def item_changed(self, item):
+        self.modified[item.row() + 1] = [item.text(), item.column()]
+
+    def add_comb(self):
+        self.message('Нажмите комбинацию клавишь')
+        hotkey = keyboard.read_hotkey(suppress=False)
+        self.message('Готово')
+        return hotkey
+
+    def add_url(self):
+        url = QFileDialog.getOpenFileName(self, 'Выбрать файл', '', 'Все файлы (*)')[0]
+        self.output[1] = url
+        self.url_btn.setText(url)
+
+    def message(self, text):
+        message = QMessageBox()
+        message.setWindowTitle("Сообщение")
+        message.setText(text)
+        message.exec_()
 
 
 if __name__ == '__main__':
